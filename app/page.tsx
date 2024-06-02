7955acf4fa0e7c14bb1b023871cf4bd06480d49a
hugomufraggi <hugo.mufraggi@epitech.eu>
2024-06-02 19:47:57 +0200

    feat add fist visualisation

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,23 @@ import CsvFrom, {FormCsvResult} from "@/components/csvFrom";
 import {useState} from "react";
 import {Label} from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import Papa from 'papaparse';
 
 
 export default function Home() {
     const [selectedFile, setSelectedFile] = useState<FormCsvResult | null>(null);
-
+    const [csvData, setCsvData] = useState<string[][]>([]);
     const handleFileChange = (res: FormCsvResult) => {
         setSelectedFile(res);
+        if (res.file) {
+            Papa.parse(res.file, {
+                complete: (result) => {
+                    console.log(result);
+                    setCsvData(result.data as string[][]);
+                },
+                header: res.marketing_emails,
+            });
+        }
     };
     return <div>
         <div>
@@ -31,5 +41,29 @@ export default function Home() {
                 </div>
             )}
         </div>
+        {csvData.length > 0 && (
+            <table className="min-w-full mt-10 bg-white">
+                <thead>
+                <tr>
+                    {csvData[0].map((header, index) => (
+                        <th key={index} className="py-2 px-4 border-b border-gray-200 bg-gray-100">
+                            {header}
+                        </th>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {csvData.slice(1).map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {row.map((cell, cellIndex) => (
+                            <td key={cellIndex} className="py-2 px-4 border-b border-gray-200">
+                                {cell}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        )}
     </div>
 }
